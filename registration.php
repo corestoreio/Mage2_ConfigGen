942d95580e7f6e894eea70a837f4b299809fe17b
Cyrill Schumacher <cyrill@schumacher.fm>
2015-12-31 11:42:04 +0100

    Update composer.json

--- registration.php
+++ registration.php
@@ -2,4 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Mage2_ConfigGen', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'CoreStore_ConfigGen', __DIR__);
